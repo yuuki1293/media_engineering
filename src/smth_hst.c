b204a3c26d0262572e19c239a724a7b4da2e0d6b
yuuki1293 <yuuki1293@outlook.jp>
2024-05-09 10:34:30 +0900

    平滑化

--- src/smth_hst.c
+++ src/smth_hst.c
@@ -2,6 +2,9 @@
 #include<stdio.h>
 #include"pgmlib.h"
 #define FINAL_LEVEL 64  /* 最終的な表現階調数 */
+long int hist[256]; /* ヒストグラム用１次元配列 */
+void make_histogram( int n );
+void make_histogram_image( int h, int n );
 
 void smooth_histgram( )
 /* image1 のヒストグラムを平滑化して image2 へ */
@@ -46,5 +49,45 @@ int main( )
     load_image( 0, ""); /* 原画像を image1 に読み込む */
     smooth_histgram( ); /* ヒストグラムを平滑化して image2 へ */
     save_image( 1, ""); /* image2 を保存する */
+    make_histogram(1);
+    make_histogram_image(256, 2);
+    save_image(2, "");
     return 0;
+}
+
+void make_histogram( int n )
+/* 画像No.n を元にヒストグラムを作る */
+{
+    int i,x,y;
+    
+    /* ヒストグラムの初期化 */
+    for(i=0;i<256;i++) hist[i] = 0;
+    /* 画像をラスタスキャンしてグラフを作る */
+    for(y=0;y<height[n];y++)
+        for(x=0;x<width[n];x++)
+            hist[ image[n][x][y] ]++; /* 累積を１増加 */
+}
+
+void make_histogram_image( int h, int n )
+/* ヒストグラムを画像化(横256×縦h画素)して */
+/* 画像No.nに記録する */
+{
+    int i,x,y;
+    long int max; /* 最大頻度 */
+    int takasa;   /* 各階調値の頻度の高さ */
+    
+    /* 画像の縦・横の値の入力 */
+    width[n]=256;  height[n]=h;
+    /* ヒストグラムの最大頻度maxを求める */
+    max=0;
+    for(i=0;i<256;i++)
+        if ( hist[i] > max ) max = hist[i];
+    /* 縦幅をh画素に正規化しながら画像に値を代入 */
+    for(x=0;x<width[n];x++){
+        takasa = (int)( h / (double)max * hist[x] );
+        if ( takasa > h ) takasa = h;
+        for(y=0;y<h;y++)
+            if ( y < takasa ) image[n][x][h-1-y] = 0;
+            else image[n][x][h-1-y] = 255;
+    }
 }
